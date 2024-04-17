15803f88931edf5f1b4e3321c90dd6fa16e15c88
Flávio Manzi Alves <manzigyn@gmail.com>
2024-04-17 15:40:24 -0300

    Tratamento de erro ao processar o arquivo

--- view/ViewJira.py
+++ view/ViewJira.py
@@ -15,64 +15,71 @@ class ViewJira():
         delimitador = st.sidebar.text_input(label="Delimitador dos campos",value=",")
         enconder = st.sidebar.selectbox(label="Codificação", options=["utf-8","latin-1"])
         if arquivoCarregado:
-            ctlJira = ctl.CTLJira(arquivoCarregado, delimitador, enconder)
-                        
-            cmbCampos = st.sidebar.multiselect(f"Campos",
-                ctlJira.obterListaCampos(),            
-                ctlJira.obterListaCamposInicial(),
-                placeholder=co.LST_SELECIONE)
             
-            optProjetos = st.sidebar.radio("Grupo de projetos", options=["Todos","Brasil","LATAM"])
+            try:
             
-            if optProjetos == "Todos":
-                lstProjetos = ctlJira.obterListaProjetos()
-            elif optProjetos == "Brasil":
-                lstProjetos = ctlJira.obterProjetoBrasil()
-            else:
-                lstProjetos = ctlJira.obterProjetoLATAM()    
-            
-            cmbProjetos = st.sidebar.multiselect(f"Projetos",
-                lstProjetos,            
-                lstProjetos,
-                placeholder=co.LST_SELECIONE)
-                        
-            chkPorcentagem = st.sidebar.checkbox("Mostrar percentual", value=True)
-            
-            
-            df_filtrado = ctlJira.filtrarProjetos(cmbProjetos) if len(cmbCampos)> 0 else ctlJira._df_tickets
-           
-                    
-            with st.container():
-                nCol = len(cmbCampos)
-                wCol = 2
-                altura = 380
-                colunas = st.columns(wCol)           
-                
-                
-                tab_analise, tab_dados = st.tabs([":clipboard: Análise", ":books: Dados"])
-                with tab_analise:
-                    tab_analise.subheader(f"Análise Quantitativa dos {len(df_filtrado.index)} registros")                     
-                    k=0
-                    for i in range(nCol):
-                        
-                        coluna = st.columns(wCol)                                
-                        with coluna[0]:
-                            df = ctlJira.agruparCampo(df_filtrado, [cmbCampos[i]], chkPorcentagem)
-                            coluna[0].subheader(cmbCampos[i])
-                            coluna[0].container(height=altura).dataframe(df, hide_index=True, use_container_width=True )
+                ctlJira = ctl.CTLJira(arquivoCarregado, delimitador, enconder)
                             
-                        if cmbCampos[i] != "Projeto":
-                            with coluna[1]:
-                                df = ctlJira.agruparCampo(df_filtrado, ["Projeto", cmbCampos[i]], chkPorcentagem)
-                                coluna[1].subheader(f"Projeto -> {cmbCampos[i]}")
-                                coluna[1].container(height=altura).dataframe(df, hide_index=True, use_container_width=True )  
-                                                      
-                with tab_dados:
-                    r_da1 = st.columns(1)            
-            
-                    r_da1[0].subheader(f"Valores originais. Total de registros: {len(ctlJira._df_tickets.index)}")
-                    r_da1[0].dataframe(ctlJira._df_tickets)
+                cmbCampos = st.sidebar.multiselect(f"Campos",
+                    ctlJira.obterListaCampos(),            
+                    ctlJira.obterListaCamposInicial(),
+                    placeholder=co.LST_SELECIONE)
                 
+                optProjetos = st.sidebar.radio("Grupo de projetos", options=["Todos","Brasil","LATAM"])
+                
+                if optProjetos == "Todos":
+                    lstProjetos = ctlJira.obterListaProjetos()
+                elif optProjetos == "Brasil":
+                    lstProjetos = ctlJira.obterProjetoBrasil()
+                else:
+                    lstProjetos = ctlJira.obterProjetoLATAM()    
+                
+                cmbProjetos = st.sidebar.multiselect(f"Projetos",
+                    lstProjetos,            
+                    lstProjetos,
+                    placeholder=co.LST_SELECIONE)
+                            
+                chkPorcentagem = st.sidebar.checkbox("Mostrar percentual", value=True)
+                
+                
+                df_filtrado = ctlJira.filtrarProjetos(cmbProjetos) if len(cmbCampos)> 0 else ctlJira._df_tickets
             
+                        
+                with st.container():
+                    nCol = len(cmbCampos)
+                    wCol = 2
+                    altura = 380
+                    colunas = st.columns(wCol)           
+                    
+                    
+                    tab_analise, tab_dados = st.tabs([":clipboard: Análise", ":books: Dados"])
+                    with tab_analise:
+                        tab_analise.subheader(f"Análise Quantitativa dos {len(df_filtrado.index)} registros")                     
+                        k=0
+                        for i in range(nCol):
+                            
+                            coluna = st.columns(wCol)                                
+                            with coluna[0]:
+                                df = ctlJira.agruparCampo(df_filtrado, [cmbCampos[i]], chkPorcentagem)
+                                coluna[0].subheader(cmbCampos[i])
+                                coluna[0].container(height=altura).dataframe(df, hide_index=True, use_container_width=True )
+                                
+                            if cmbCampos[i] != "Projeto":
+                                with coluna[1]:
+                                    df = ctlJira.agruparCampo(df_filtrado, ["Projeto", cmbCampos[i]], chkPorcentagem)
+                                    coluna[1].subheader(f"Projeto -> {cmbCampos[i]}")
+                                    coluna[1].container(height=altura).dataframe(df, hide_index=True, use_container_width=True )  
+                                                        
+                    with tab_dados:
+                        r_da1 = st.columns(1)            
+                
+                        r_da1[0].subheader(f"Valores originais. Total de registros: {len(ctlJira._df_tickets.index)}")
+                        r_da1[0].dataframe(ctlJira._df_tickets)
+            except  UnicodeDecodeError as e:
+                st.error(f"Falha no processamento do arquivo. Verifique o delimitador e o enconding apropriado. Mensagem {e}")       
+            except LookupError as e:
+                st.error(f"Falha no processamento do arquivo. Verifique o enconding apropriado. Mensagem {e}")       
+            except:
+                st.error(f"Falha no processamento do arquivo. Verifique o delimitador e o enconding apropriado. Mensagem {e}")       
         
     
